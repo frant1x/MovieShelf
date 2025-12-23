d67132ff143cc2682848e1ec121328f952d813a6
Nazar Pyskor <pyskornazik@gmail.com>
2025-12-23 08:44:52 +0200

    Updated users view and serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -12,3 +12,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
