b217fb4715d1849336727535cb6b1cede7d07998
Nazar Pyskor <pyskornazik@gmail.com>
2025-12-29 19:59:18 +0200

    Added basic routing and Home placeholder

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import { Container, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom'; // Імпортуємо NavLink
+import logo from '../assets/images/logo.png';
+
+const Header = () => {
+  return (
+    <div>Home</div>
+  );
+};
+
+export default Header;
